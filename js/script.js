4c041f438c6c84fc44a15a0de05aa1d198449fa0
Aminat Ameen <ameenaminatmorounmubo@gmail.com>
2023-09-13 02:48:28 +0100

    added the division and multiplication operators

--- js/script.js
+++ js/script.js
@@ -1,57 +1,110 @@
+let currentOperand = "";
+let previousOperand = "";
+let currentOperator = null;
 
-let numbers = document.getElementsByClassName('btn-number');
-let input = document.getElementById('input');
-let operators = document.getElementsByClassName('btn-operator');
-let equals = document.getElementById('equal');
-let reset = document.querySelector('.rest td');
-
+let numbers = document.querySelectorAll(".btn-number");
+let input = document.getElementById("input");
+let operators = document.getElementsByClassName("btn-operator");
+let equals = document.getElementById("equal");
+let reset = document.querySelector(".rest td");
+let clear = document.getElementById("clear");
 /* Store the current input */
-let currentInput = '';
+let currentInput = "";
 /* Store the current operator */
-let currentOperator = '';
+// let currentOperator = '';
 /* Store the current calculation */
 let calculation = null;
 
-/* Add the listeners to the numbers */
-for (let i = 0; i < numbers.length; i++) {
-    let number = numbers[i];
-    number.addEventListener('click', function () {
-        currentInput += this.innerHTML;
-        input.innerHTML = currentInput;
-    });
+// Function to update the display
+function updateDisplay() {
+  input.innerText =
+    previousOperand +
+    (currentOperator ? " " + currentOperator : "") +
+    (currentOperand ? " " + currentOperand : "");
 }
+
+// Function to append a digit to the current operand
+function appendNumber(number) {
+  currentOperand += number;
+  updateDisplay();
+}
+
+/* Add the listeners to the numbers */
+numbers.forEach((button) => {
+  button.addEventListener("click", () => {
+    appendNumber(button.innerText);
+  });
+});
 
 /* Added listeners to the operators */
 for (let i = 0; i < operators.length; i++) {
-    let operator = operators[i];
-    operator.addEventListener('click', function () {
-        /* If the user presses an operator we will need to store the current calculation and operator */
-        if (calculation == null) {
-            calculation = Number(currentInput);
-        } else {
-            /*  current operation */
-            if (currentOperator == '+') {
-                calculation += Number(currentInput);
-            } else if (currentOperator == '-') {
-                calculation -= Number(currentInput);
-            }
+  let operator = operators[i];
+  operator.addEventListener("click", function () {
+    /* If the user presses an operator we will need to store the current calculation and operator */
+    if (currentOperand !== "") {
+      if (previousOperand === "") {
+        previousOperand = currentOperand;
+      } else {
+        /*  current operation */
+        const num1 = Number(previousOperand);
+        const num2 = Number(currentOperand);
+        if (currentOperator === "+") {
+          calculation = num1 + num2;
+        } else if (currentOperator === "-") {
+          calculation = num1 - num2;
+        } else if (currentOperator === "*") {
+          calculation = num1 * num2;
+        } else if (currentOperator === "/") {
+          calculation = num1 / num2;
         }
-        currentOperator = this.innerHTML;
-        currentInput = '';
-        input.innerHTML = '';
-    });
+        previousOperand = calculation;
+      }
+
+      currentOperator = this.innerHTML;
+      currentOperand = "";
+      //   input.innerHTML = "";
+      updateDisplay();
+    }
+  });
 }
 
 /*  equals press */
-equals.addEventListener('click', function () {
-    /*  current operation */
-    if (currentOperator == '+') {
-        calculation += Number(currentInput);
-    } else if (currentOperator == '-') {
-        calculation -= Number(currentInput);
+equals.addEventListener("click", function () {
+  /*  current operation */
+  if (currentOperand !== "") {
+    const num1 = Number(previousOperand);
+    const num2 = Number(currentOperand);
+    if (currentOperator === "+") {
+      calculation = num1 + num2;
+    } else if (currentOperator === "-") {
+      calculation = num1 - num2;
+    } else if (currentOperator === "*") {
+      calculation = num1 * num2;
+    } else if (currentOperator === "/") {
+      calculation = num1 / num2;
     }
-    input.innerHTML = calculation;
-    currentInput = '' + calculation;
-    calculation = null;
-    currentOperator = '';
+  }
+  input.innerHTML = calculation;
+  currentOperand = "" + calculation;
+  currentOperator = "";
+  previousOperand = "";
+
+  updateDisplay();
 });
+
+// ...
+
+/* Add a listener to the backspace button */
+
+clear.addEventListener("click", function () {
+  if (currentOperand) {
+    currentOperand = currentOperand.slice(0, -1);
+  } else if (currentOperator) {
+    currentOperator = null;
+  } else if (previousOperand) {
+    previousOperand = "";
+  }
+  updateDisplay();
+});
+
+updateDisplay();
